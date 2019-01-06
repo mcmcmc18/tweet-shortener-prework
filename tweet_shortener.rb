d55b0be3b8108016c8969311ed5db8bdc30d7568
MC Simoes <mariacristina.simoes@gmail.com>
2019-01-06 03:45:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,7 +40,7 @@ end
 def shortened_tweet_truncator(tweet)
   tweet.split(" ").map do |phrase|
     if phrase.length > 140
-      word_substituter(phrase).truncate(140) + "..."
+      word_substituter(phrase)[0..136] + "..."
     else
       phrase
     end
